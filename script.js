ad5cf33e006baf54d7b08452563075464b656b2f
Bhavitha <chinthabhavitha6@gmail.com>
2024-09-01 14:57:31 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+let change=document.querySelector(".change");
+var countDownDate=new Date("September 26,2024 00:00:00").getTime();
+var x= setInterval(function(){
+    var now=new Date().getTime();
+    var distance=countDownDate-now;
+
+    var days=Math.floor(distance/(1000*60*60*24));
+    var hours=Math.floor((distance%(1000*60*60*24))/(1000*60*60));
+    var minutes=Math.floor((distance%(1000*60*60))/(1000*60));
+    var seconds=Math.floor((distance%(1000*60))/1000);
+
+    document.querySelector("#days").innerHTML=days;
+    document.querySelector("#hours").innerHTML=hours;
+   document.querySelector("#minutes").innerHTML= minutes;
+    document.querySelector("#seconds").innerHTML=seconds;
+
+
+},1000)
+ 
